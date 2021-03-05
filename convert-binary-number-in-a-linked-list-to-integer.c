22d6d745b770c5e0eee2ccc491d08b7e5e8ee771
eshekak <olenev.biz@gmail.com>
2021-03-05 20:33:16 +0300

    convert-binary-number-in-a-linked-list-to-integer: done

--- convert-binary-number-in-a-linked-list-to-integer.c
+++ convert-binary-number-in-a-linked-list-to-integer.c
@@ -1,3 +1,13 @@
+/**
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+Input: head = [1,0,1]
+Output: 5
+Explanation: (101) in base 2 = (5) in base 10
+**/
+
 #include <stdio.h>
 
 struct ListNode {
@@ -6,7 +16,14 @@ struct ListNode {
 };
 
 int getDecimalValue(struct ListNode* head){
-	return head->val;
+	int result = head->val;
+	
+	while (head->next != NULL) {
+		result = (result << 1) | head->next->val;
+		head = head->next;
+	}
+
+	return result;
 }
 
 int main() {
