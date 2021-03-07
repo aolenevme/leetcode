2d481e04d3e115c4029664a725c93b4d25762c7d
eshekak <olenev.biz@gmail.com>
2021-03-07 15:22:15 +0300

    count-negative-numbers-in-a-sorted-matrix: done

--- count-negative-numbers-in-a-sorted-matrix.c
+++ count-negative-numbers-in-a-sorted-matrix.c
@@ -9,34 +9,39 @@ Explanation: There are 8 negatives number in the matrix.
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int countNegatives(int** grid, int gridSize, int* gridColSize){
-	int x = gridSize - 1,
-	    y = 0,
-	    counter = 0;
+        int x = gridSize - 1,
+            y = 0,
+            counter = 0;
 
-	while (x >= 0 && y < *gridColSize) {
-		if (grid[y][x] < 0) {
-			counter += *gridColSize - y;
-			x -= 1;
-		} else {
-			y += 1;
-		}
-	}
+        while (x >= 0 && y < *gridColSize) {
+                if (grid[x][y] < 0) {
+                        counter += *gridColSize - y;
+                        x -= 1;
+                } else {
+                        y += 1;
+                }
+        }
 
-	return counter;
+        return counter;
 }
 
 int main() {
-	int grid[4][4] = {
-		{4,3,2,-1},
-		{3,2,1,-1},
-		{1,1,-1,-2},
-		{-1,-1,-2,-3}
-	}; 
+	int gridSize = 2;
+	int gridColSize = 2;
+	
+	int **grid = (int **) malloc(gridSize * sizeof(int));
 
-	int gridSize = 4;
-	int gridColSize = 4;
+	for (int i = 0; i < gridColSize; i++) {
+		grid[i] = (int *) malloc(gridColSize * sizeof(int));
+	}
+
+	grid[0][0] = 0;
+	grid[0][1] = -1;
+	grid[1][0] = -2;
+	grid[1][1] = -3;
 
 	printf("%d", countNegatives(grid, gridSize, &gridColSize)); 
 }
