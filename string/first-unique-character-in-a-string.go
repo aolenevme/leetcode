a023cc01cb133c215a4fcd6d5b61928b4726f9d9
Alexander Olenev <hello@aolenev.me>
2021-12-02 14:54:40 +0300

    string/first-unique-character-in-a-string: done

---
+++ string/first-unique-character-in-a-string.go
@@ -0,0 +1,49 @@
+/*
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(firstUniqChar("leetcode"))
+	fmt.Println(firstUniqChar("loveleetcode"))
+	fmt.Println(firstUniqChar("aabb"))
+}
+
+func firstUniqChar(s string) int {
+	kv := make(map[rune]int)
+
+	for _, v := range []rune(s) {
+		kv[v]++
+	}
+
+	for i, v := range []rune(s) {
+		if kv[v] == 1 {
+			return i
+		}
+	}
+
+	return -1
+}
