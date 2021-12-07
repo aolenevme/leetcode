7b2e4029d888fbe2d38d7c087242bb5efbd18ab3
Alexander Olenev <hello@aolenev.me>
2021-12-07 15:26:40 +0300

    two-pointers/reverse-words-in-a-string-iii: done

--- two-pointers/two-sum-ii-input-array-is-sorted.go
+++ two-pointers/two-sum-ii-input-array-is-sorted.go
@@ -39,15 +39,15 @@ import (
 )
 
 func main() {
-	fmt.Println(twoSum([]int{2,7,11,15}, 9))
-	fmt.Println(twoSum([]int{2,3,4}, 6))
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum([]int{2, 3, 4}, 6))
 	fmt.Println(twoSum([]int{-1, 0}, -1))
 }
 
 func twoSum(numbers []int, target int) []int {
 	result := make([]int, 2)
 
-	for start, end := 0, len(numbers) - 1; start < end; {
+	for start, end := 0, len(numbers)-1; start < end; {
 		test := numbers[start] + numbers[end]
 
 		if test > target {
