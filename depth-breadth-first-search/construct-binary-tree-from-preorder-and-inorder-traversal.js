16edeb04647eab945585f8a2e973a5a6c5d1be26
Alexander Olenev <hello@aolenev.me>
2022-06-21 14:01:04 +0400

    construct-binary-tree-from-preorder-and-inorder-traversal

---
+++ depth-breadth-first-search/construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -0,0 +1,63 @@
+/**
+105. Construct Binary Tree from Preorder and Inorder Traversal
+
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+Example 1:
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+
+Example 2:
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+
+Constraints:
+1 <= preorder.length <= 3000
+inorder.length == preorder.length
+-3000 <= preorder[i], inorder[i] <= 3000
+preorder and inorder consist of unique values.
+Each value of inorder also appears in preorder.
+preorder is guaranteed to be the preorder traversal of the tree.
+inorder is guaranteed to be the inorder traversal of the tree.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    const hash = {};
+    
+    let preorderIdx = 0;
+    
+    inorder.forEach((value, index) => {
+        hash[value] = index;
+    });
+    
+    function recurse(left, right) {
+        if (left > right) {
+            return null;
+        }
+        
+        const value = preorder[preorderIdx]; 
+        const root = new TreeNode(value);
+        
+        preorderIdx++;
+        
+        root.left = recurse(left, hash[value] - 1);
+        root.right = recurse(hash[value] + 1, right);
+        
+        return root;
+    }
+    
+    return recurse(0, preorder.length - 1);
+};
