0a73e37fd9d8d68fb55058d1990327d6cb67d84e
Alexander Olenev <hello@aolenev.me>
2022-07-24 15:54:13 +0300

    done

--- array/3sum.js
+++ array/3sum.js
@@ -8,8 +8,9 @@ Notice that the solution set must not contain duplicate triplets.
 Example 1:
 Input: nums = [-1,0,1,2,-1,-4]
 Output: [[-1,-1,2],[-1,0,1]]
+
 Explanation: 
-nums[0] + nums[1] + nums[1] = (-1) + 0 + 1 = 0.
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
 nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
 nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
 The distinct triplets are [-1,0,1] and [-1,-1,2].
@@ -30,13 +31,47 @@ Constraints:
 -105 <= nums[i] <= 105
 */
 
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-var threeSum = function(nums) {
-    // 1. сортируем массив
-    // 2. три указателя: i < j < k
-    // 3. далее начинаем двигать j++, пока не закончится массив. следом начинаем двигать i++. k же делает бинарный поиск. k ищет число, которое равно = nums[i] + nums[j]. если такое находится, то добавляем результат в Set (как-то :)) )
-    // 4. возвращаем этот Set
+function threeSum(nums) {
+	const result = [];
+
+	nums.sort((a, b) => a - b);
+
+	for (let start = 0; start < nums.length - 2; start++) {
+		if (nums[start] > 0) {
+			break;
+		}
+
+		if (nums[start] === nums[start - 1]) {
+			continue;
+		}
+
+
+		let middle = start + 1;
+		let end = nums.length - 1;
+
+		while (middle < end) {
+			let sum = nums[start] + nums[middle] + nums[end];
+
+			if (sum === 0) {
+				result.push([nums[start], nums[middle], nums[end]])
+
+				while (nums[middle] === nums[middle + 1]) {
+					middle++;
+				}
+
+				while (nums[end] === nums[end - 1]) {
+					end--;
+				}
+
+				middle++;
+				end--;
+			} else if (sum < 0) {
+				middle++;
+			} else {
+				end--;
+			}
+		}
+	}
+
+	return result;
 };
