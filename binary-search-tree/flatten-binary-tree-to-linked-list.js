cd21c83d09d7ca147caf142b1f2aa8e93bc1ba74
Alexander Olenev <hello@aolenev.me>
2022-10-31 23:30:40 +0200

    flatten-binary-tree-to-linked-list

---
+++ binary-search-tree/flatten-binary-tree-to-linked-list.js
@@ -0,0 +1,61 @@
+/**
+70. Flatten Binary Tree to Linked List
+
+Given the root of a binary tree, flatten the tree into a "linked list":
+
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+Example 1:
+Input: root = [1,2,5,3,4,null,6]
+Output: [1,null,2,null,3,null,4,null,5,null,6]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [0]
+Output: [0]
+
+Constraints:
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+ 
+Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function (root) {
+  const preorder = traverse(root);
+
+  let previous = preorder[0];
+
+  for (let i = 1; i < preorder.length; i++) {
+    const current = preorder[i];
+
+    previous.left = null;
+    previous.right = current;
+
+    previous = current;
+  }
+};
+
+const traverse = (node) => {
+  if (node === null) {
+    return [];
+  }
+
+  return [node, ...traverse(node.left), ...traverse(node.right)];
+};
