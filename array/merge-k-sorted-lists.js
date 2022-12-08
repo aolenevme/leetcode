1da5ba02a1ee5d6add496f185b8707293f70be53
Alexander Olenev <hello@aolenev.me>
2022-12-08 23:57:57 +0200

    merge-k-sorted-lists

---
+++ array/merge-k-sorted-lists.js
@@ -0,0 +1,77 @@
+/**
+23. Merge k Sorted Lists
+
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+Example 1:
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+
+Example 2:
+Input: lists = []
+Output: []
+
+Example 3:
+Input: lists = [[]]
+Output: []
+
+Constraints:
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-104 <= lists[i][j] <= 104
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 104.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists) {
+  const values = [];
+
+  for (let list of lists) {
+    while (list !== null) {
+      values.push(list.val);
+
+      list = list.next;
+    }
+  }
+
+  if (!values.length) {
+    return null;
+  }
+
+  values.sort((a, b) => a - b);
+
+  let head = new ListNode(values[0]);
+  let previous = head;
+
+  for (let i = 1; i < values.length; i++) {
+    const value = values[i];
+
+    previous.next = new ListNode(value);
+
+    previous = previous.next;
+  }
+
+  return head;
+};
