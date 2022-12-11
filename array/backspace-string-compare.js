85dd7fb412232d0c7f524b6fa04aa157be92bdcc
Alexander Olenev <hello@aolenev.me>
2022-12-11 23:59:39 +0200

    backspace-string-compare

---
+++ array/backspace-string-compare.js
@@ -0,0 +1,64 @@
+/**
+844. Backspace String Compare
+
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+Example 1:
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+
+Example 2:
+Input: s = "ab##", t = "c#d#"
+Output: true
+Explanation: Both s and t become "".
+
+Example 3:
+Input: s = "a#c", t = "b"
+Output: false
+Explanation: s becomes "c" while t becomes "b".
+
+Constraints:
+1 <= s.length, t.length <= 200
+s and t only contain lowercase letters and '#' characters.
+ 
+
+Follow up: Can you solve it in O(n) time and O(1) space?
+*/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function (s, t) {
+  let sResult = [];
+  for (const char of s) {
+    if (char === "#") {
+      const i = sResult.length - 1;
+
+      sResult.splice(i, 1);
+    } else {
+      sResult.push(char);
+    }
+  }
+
+  let tResult = [];
+  for (const char of t) {
+    if (char === "#") {
+      const i = tResult.length - 1;
+
+      tResult.splice(i, 1);
+    } else {
+      tResult.push(char);
+    }
+  }
+
+  const sStringified = sResult.join("");
+  const tStringified = tResult.join("");
+  const result = sStringified === tStringified;
+
+  return result;
+};
