32668edd3e7b5c6c407f46c0c531e6a00e161000
Alexander Olenev <hello@aolenev.me>
2023-01-29 19:14:10 +0200

    group-anagrams

---
+++ array/group-anagrams.js
@@ -0,0 +1,79 @@
+/**
+49. Group Anagrams
+
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+
+Constraints:
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+*/
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  const grouped = {};
+
+  for (const str of strs) {
+    const counters = {
+      a: 0,
+      b: 0,
+      c: 0,
+      d: 0,
+      e: 0,
+      f: 0,
+      g: 0,
+      h: 0,
+      i: 0,
+      j: 0,
+      k: 0,
+      l: 0,
+      m: 0,
+      n: 0,
+      o: 0,
+      p: 0,
+      q: 0,
+      r: 0,
+      s: 0,
+      t: 0,
+      u: 0,
+      v: 0,
+      w: 0,
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+
+    for (const char of str) {
+      counters[char]++;
+    }
+
+    const key = JSON.stringify(counters);
+
+    if (key in grouped) {
+      grouped[key].push(str);
+    } else {
+      grouped[key] = [str];
+    }
+  }
+
+  const anagrams = Object.values(grouped);
+
+  return anagrams;
+};
