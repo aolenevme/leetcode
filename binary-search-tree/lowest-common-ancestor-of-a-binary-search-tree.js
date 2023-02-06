9c81c27684213a16755c2b4d9de4fb6928131255
Alexander Olenev <hello@aolenev.me>
2023-02-06 18:44:41 +0200

    lowest-common-ancestor-of-a-binary-search-tree

---
+++ binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,85 @@
+/**
+235. Lowest Common Ancestor of a Binary Search Tree
+
+Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Example 1:
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+
+Example 2:
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+
+Example 3:
+Input: root = [2,1], p = 2, q = 1
+Output: 2
+
+Constraints:
+The number of nodes in the tree is in the range [2, 105].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the BST.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  const pathP = collectPath(root, p, [root]);
+  const pathQ = collectPath(root, q, [root]);
+
+  const length = Math.min(pathP.length, pathQ.length);
+
+  let result = null;
+
+  for (let i = 0; i < length; i++) {
+    const pNode = pathP[i];
+    const qNode = pathQ[i];
+
+    if (pNode === qNode) {
+      result = pNode;
+
+      continue;
+    } else {
+      break;
+    }
+  }
+
+  return result;
+};
+
+const collectPath = (node, nodeToFind, path) => {
+  if (node === null) {
+    return null;
+  }
+
+  if (node === nodeToFind) {
+    return path;
+  }
+
+  const leftPath = collectPath(node.left, nodeToFind, [...path, node.left]);
+  const rightPath = collectPath(node.right, nodeToFind, [...path, node.right]);
+
+  if (leftPath) {
+    return leftPath;
+  }
+
+  return rightPath;
+};
