f7cd3f02d0d440f3044ff73014c3aa6780d8c838
Alexander Olenev <hello@aolenev.me>
2023-02-11 22:45:58 +0200

    search-a-2d-matrix

---
+++ matrix/search-a-2d-matrix.js
@@ -0,0 +1,52 @@
+/**
+74. Search a 2D Matrix
+
+You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+
+Example 2:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104
+*/
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+  const array = matrix.flatMap((num) => num);
+
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    const middle = Math.round((left + right) / 2);
+    const value = array[middle];
+
+    if (target === value) {
+      return true;
+    } else if (value > target) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+
+  return false;
+};
